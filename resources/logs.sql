d850ae056639e45c0fbcff68492ea0e2ff06418f
John Skilbeck <john.skilbeck@gmail.com>
2018-03-12 00:27:20 -0700

    Add portfolio athena query. Add logs query

---
+++ resources/logs.sql
@@ -0,0 +1,15 @@
+with dates as (
+  select
+    '2018-03-01'::date as _start,
+    '2018-10-31'::date as _end
+)
+select * from crosstab (
+  'select date,device,count(*)
+   from dw.log
+   group by 1,2
+   order by 1,2',
+  'select distinct device from dw.log order by 1'
+)  as ct (date date,firewall int,pfsense int,router int)
+where date between (select _start from dates)
+               and (select _end   from dates)
+order by 1 desc;
--- resources/portfolio_athena.sql
+++ resources/portfolio_athena.sql
@@ -1,13 +1,23 @@
 with now as (
-  select cast(current_timestamp at time zone 'America/Los_Angeles' as date) as now
+  -- select cast(current_timestamp at time zone 'America/Los_Angeles' as date) as now
+  select cast('2018-03-08' as date) as now
 ), date as (
   select
     (select now from now) today,
-    case day_of_week(current_timestamp at time zone 'America/Los_Angeles') % 7
+    -- case day_of_week(current_timestamp at time zone 'America/Los_Angeles') % 7
+    case day_of_week(cast('2018-03-07' as date)) % 7
       when 1 then (select now from now) - interval '3' day
       when 0 then (select now from now) - interval '2' day
       else        (select now from now) - interval '1' day
     end as yesterday
+), _portfolio as (
+  select
+    dataset,
+    ticker,
+    cast(quantity as decimal(10,4))      as quantity,
+    cast(cost_per_share as decimal(6,2)) as cost_per_share
+  from
+    dw.portfolio
 ), _equities as (
   select
     dataset,
@@ -28,8 +38,61 @@ with now as (
     dw.equities
 ), today as (
   select
-    *
+    markets.description,
+    markets.ticker,
+    sum((quantity * cost_per_share))                        cost_basis,
+    sum((quantity * close))                                 market_value,
+    sum(((quantity * close) - (quantity * cost_per_share))) gain_loss
   from
-    _equities
+    _equities equities
+    join _portfolio portfolio on equities.dataset = portfolio.dataset and equities.ticker = portfolio.ticker
+    join dw.markets on equities.dataset = markets.dataset and equities.ticker = markets.ticker
+  where
+    date in ( select today from date )
+  group by
+    1,2
+), yesterday as (
+  select
+    markets.description,
+    markets.ticker,
+    sum((quantity * close))                                 yesterday
+  from
+    _equities equities
+    join _portfolio portfolio on equities.dataset = portfolio.dataset and equities.ticker = portfolio.ticker
+    join dw.markets on equities.dataset = markets.dataset and equities.ticker = markets.ticker
+  where
+    date in ( select yesterday from date )
+  group by
+    1,2
+), detail as (
+  select
+    today.description,
+    today.cost_basis, today.market_value, today.gain_loss,
+    today.market_value - yesterday.yesterday today_gain_loss
+  from
+    today
+    join yesterday on today.ticker = yesterday.ticker
+), summary as (
+  select
+    'Portfolio Total'       description,
+    sum(cost_basis)         cost_basis,
+    sum(market_value)       market_value,
+    sum(gain_loss)          gain_loss,
+    sum(today_gain_loss)    today_gain_loss
+  from
+    detail
+), _union as (
+  select * from summary
+  union all
+  select * from detail
+), report as (
+  select
+    description, cast(cost_basis as integer) cost_basis, cast(market_value as integer) market_value,
+    cast(today_gain_loss as integer) today_gain_loss,
+    cast(cast((today_gain_loss / market_value * 100) as decimal(8,2)) as varchar) || '%'  "today_gain_loss_%",
+    cast(gain_loss as integer) total_gain_loss,
+    cast(cast((gain_loss / market_value * 100) as decimal(8,2)) as varchar) || '%'  "total_gain_loss_%"
+  from
+    _union
 )
-select * from today limit 50
+select * from report
