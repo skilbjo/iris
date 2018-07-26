af83d8f270d7393ee78bc18567fe025a1d00bc9f
John Skilbeck <john.skilbeck@gmail.com>
2018-07-25 23:52:46 -0700

    update to query for new device

--- resources/logs.sql
+++ resources/logs.sql
@@ -1,6 +1,6 @@
 with dates as (
   select
-    '2018-03-01'::date as _start,
+    '2018-07-20'::date as _start,
     '2018-10-31'::date as _end
 )
 select * from crosstab (
@@ -9,7 +9,7 @@ select * from crosstab (
    group by 1,2
    order by 1,2',
   'select distinct device from dw.log order by 1'
-)  as ct (date date,firewall int,pfsense int,router int)
+)  as ct (date date,firewall int, pfsense int, pfsense2 int, router int)
 where date between (select _start from dates)
                and (select _end   from dates)
 order by 1 desc;
