67d95b06cff5e87df9d07fb941d4e3116457ffbd
John Skilbeck <john.skilbeck@gmail.com>
2018-10-19 19:14:37 -0700

    Add FC

--- resources/portfolio.sql
+++ resources/portfolio.sql
@@ -25,11 +25,13 @@ with now as (
 ), beginning_of_year as (
   select date_trunc('year', ( select now from now)) + interval '1 day' beginning_of_year
     --  select '2018-01-02' beginning_of_year
+), fx as (
+  select 'GBP'::text as currency, 1.31 rate
 ), equities as (
   select
     ticker,
     date,
-    avg(close) as close
+    avg(case when ticker = 'LON:FCH' then close * (select rate from fx where currency = 'GBP') / 100 else close end) as close
   from
     dw.equities_fact
   where
